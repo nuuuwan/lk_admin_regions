96169bbf8718c6244ddf4ab57c044e1eb170b18c
nuuuwan <nuuuwan@gmail.com>
2025-12-07 16:02:52 +0530

    Refactor BuildEnts and BuildGeo modules; add fuzzy matching and merge logic for GNDs

--- workflows/pipeline.py
+++ workflows/pipeline.py
@@ -1,7 +1,9 @@
-from lk_admin_regions import BuildGeo
+from lk_admin_regions import BuildEnts
 
 if __name__ == "__main__":
     # BuildEnts.build_all()
     # BuildGeo.build_all()
     # BuildGeo.HACK_delete_large_files()
-    BuildGeo.validate()
+    # BuildGeo.validate()
+
+    BuildEnts.merge_pds()
