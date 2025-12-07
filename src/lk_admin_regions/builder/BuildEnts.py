5ca8996b2f8c25ccd072e1d5b21c308d02c98531
nuuuwan <nuuuwan@gmail.com>
2025-12-07 14:10:00 +0530

    Add ID correction map and correction methods for DSDs and GNDs in LKAAdminBoundariesXLSX

--- src/lk_admin_regions/builder/BuildEnts.py
+++ src/lk_admin_regions/builder/BuildEnts.py
@@ -19,33 +19,6 @@ class BuildEnts:
         ("gnd", 4, 7),
     ]
 
-    # IDs assigned to newly created DSDs and GNDs (see README.recent-history.md)
-    # in humdata is inconsistent with how http://moha.gov.lk
-    # and others define them.
-    # This map is used to correct those IDs.
-    ID_CORRECTION_MAP = {
-        # LK-23
-        # "LK-2303": "LK-2303",  # Kothmale East - UNCHANGED
-        "LK-2321": "LK-2304",  # Kothmale West
-        # "LK-2306": "LK-2306",  # Hanguranketha - UNCHANGED
-        "LK-2318": "LK-2307",  # Mathurata
-        "LK-2324": "LK-2309",  # Walapane
-        "LK-2309": "LK-2310",  # Niladandahinna
-        # "LK-2312": "LK-2312",  # Nuwara-Eliya - UNCHANGED
-        "LK-2327": "LK-2313",  # Thalawakelle
-        "LK-2315": "LK-2315",  # Ambagamuwa Korale - UNCHANGED
-        "LK-2330": "LK-2316",  # Norwood
-        # LK-31
-        # "LK-3136": "LK-3136",  # Hikkaduwa - UNCHANGED
-        "LK-3163": "LK-3137",  # Rathgama
-        "LK-3160": "LK-3138",  # Madampagama
-        # "LK-3127": "LK-3127",  # Baddegama - UNCHANGED
-        "LK-3157": "LK-3128",  # Wanduramba
-        # LK-91
-        # "LK-9118": "LK-9118",  # Balangoda - UNCHANGED
-        "LK-9154": "LK-9119",  # Kaltota
-    }
-
     @classmethod
     def get_id_field(cls, level):
         return f"adm{level}_pcode"
