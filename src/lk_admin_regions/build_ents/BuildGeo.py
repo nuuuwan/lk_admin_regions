1cfb510bfbd3709e2f26c509faf74193e0d7e488
nuuuwan <nuuuwan@gmail.com>
2025-12-07 10:53:01 +0530

    new labels for compressed

--- src/lk_admin_regions/build_ents/BuildGeo.py
+++ src/lk_admin_regions/build_ents/BuildGeo.py
@@ -12,11 +12,13 @@ log = Log("ModuleName")
 class BuildGeo:
     DIR_DATA = BuildEnts.DIR_DATA
     DIR_DATA_GEO = os.path.join(DIR_DATA, "geo")
+    DIR_DATA_GEOJSON = os.path.join(DIR_DATA_GEO, "geojson")
+    DIR_DATA_JSON = os.path.join(DIR_DATA_GEO, "json")
     MAX_FILE_SIZE_M = 25
 
     @classmethod
     def get_ent_geojson_path(cls, dir_name_simplified, ent_type_name):
-        dir_geo = os.path.join(cls.DIR_DATA_GEO, dir_name_simplified)
+        dir_geo = os.path.join(cls.DIR_DATA_GEOJSON, dir_name_simplified)
         os.makedirs(dir_geo, exist_ok=True)
         return os.path.join(
             dir_geo,
@@ -53,7 +55,7 @@ class BuildGeo:
                     + f" {File(geojson_path)} is too large."
                 )
 
-            # cls.build_multipolygon_json(ent_type_name, level, id_len)
+            cls.build_multipolygon_json(ent_type_name, level, id_len)
             cls.build_small_geojson(ent_type_name, level)
 
     @classmethod
@@ -81,21 +83,26 @@ class BuildGeo:
                         [[point[0], point[1]] for point in ring]
                     )
 
-            dir_data_geo_ents = os.path.join(
-                cls.DIR_DATA_GEO, f"{ent_type_name}s"
+            dir_data_geo_json_ents = os.path.join(
+                cls.DIR_DATA_JSON, f"{ent_type_name}s"
             )
+            os.makedirs(dir_data_geo_json_ents, exist_ok=True)
 
             json_file = JSONFile(
-                os.path.join(dir_data_geo_ents, f"{ent_id}.json")
+                os.path.join(dir_data_geo_json_ents, f"{ent_id}.json")
             )
-            json_file.write(flattened_coordinates)
-            log.info(f"✅ Wrote {json_file}")
+            if not json_file.exists:
+                json_file.write(flattened_coordinates)
+                log.info(f"✅ Wrote {json_file}")
 
     @classmethod
     def build_small_geojson(cls, ent_type_name, level, tolerance=0.001):
 
-        for minus_log10_tolerance in [2, 3, 4]:
-            tolerance = 10 ** (-minus_log10_tolerance)
+        for [tolerance, label] in [
+            [0.0001, "small"],
+            [0.001, "smaller"],
+            [0.01, "smallest"],
+        ]:
             geojson_path = cls.get_ground_truth_geojson_path(level)
             geojson_data = JSONFile(geojson_path).read()
 
@@ -116,26 +123,28 @@ class BuildGeo:
             }
 
             simplified_geojson_file = JSONFile(
-                cls.get_ent_geojson_path(
-                    f"simplified{minus_log10_tolerance}", ent_type_name
-                )
+                cls.get_ent_geojson_path(label, ent_type_name)
             )
-            simplified_geojson_file.write(simplified_geojson)
+            if not simplified_geojson_file.exists:
+                simplified_geojson_file.write(simplified_geojson)
 
-            size_before = os.path.getsize(geojson_path)
-            size_after = os.path.getsize(simplified_geojson_file.path)
-            compression_p = size_after / size_before
+                size_before = os.path.getsize(geojson_path)
+                size_after = os.path.getsize(simplified_geojson_file.path)
+                compression_p = size_after / size_before
 
-            if simplified_geojson_file.size > cls.MAX_FILE_SIZE_M * 1_000_000:
-                log.warning(
-                    f"⚠️  Not writing {simplified_geojson_file}."
-                    + f" Size {simplified_geojson_file.size / 1_000_000:.1f}MB"
-                    + " is too large even after simplification"
-                    + f" with tolerance={tolerance}."
-                )
-                os.remove(simplified_geojson_file.path)
-            else:
-                log.info(
-                    f"✅ Wrote {simplified_geojson_file}"
-                    + f" ({compression_p:.1%} of original)"
-                )
+                if (
+                    simplified_geojson_file.size
+                    > cls.MAX_FILE_SIZE_M * 1_000_000
+                ):
+                    log.warning(
+                        f"⚠️  Not writing {simplified_geojson_file}."
+                        + f" Size {simplified_geojson_file.size / 1_000_000:.1f}MB"
+                        + " is too large even after simplification"
+                        + f" with tolerance={tolerance}."
+                    )
+                    os.remove(simplified_geojson_file.path)
+                else:
+                    log.info(
+                        f"✅ Wrote {simplified_geojson_file}"
+                        + f" ({compression_p:.1%} of original)"
+                    )
