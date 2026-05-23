1940dd70322b3792f120cfd1d3f7f45bf49ea95d
nuuuwan <nuuuwan@gmail.com>
2026-05-23 07:42:21 +0530

    Refactor ID_CORRECTION_MAP in LKAAdminBoundariesXLSX and update correction logic in temp_analyze.py

--- src/lk_admin_regions/ground_truth/humdata/LKAAdminBoundariesXLSX.py
+++ src/lk_admin_regions/ground_truth/humdata/LKAAdminBoundariesXLSX.py
@@ -16,28 +16,7 @@ class LKAAdminBoundariesXLSX:
     # in humdata is inconsistent with how http://moha.gov.lk
     # and others define them.
     # This map is used to correct those IDs.
-    ID_CORRECTION_MAP = {
-        # LK23
-        # "LK2303": "LK2303",  # Kothmale East - UNCHANGED
-        "LK2321": "LK2304",  # Kothmale West
-        # "LK2306": "LK2306",  # Hanguranketha - UNCHANGED
-        "LK2318": "LK2307",  # Mathurata
-        "LK2309": "LK2310",  # Niladandahinna # ORDER MATTERS
-        "LK2324": "LK2309",  # Walapane # ORDER MATTERS
-        # "LK2312": "LK2312",  # Nuwara-Eliya - UNCHANGED
-        "LK2327": "LK2313",  # Thalawakelle
-        # "LK2315": "LK2315",  # Ambagamuwa Korale - UNCHANGED
-        "LK2330": "LK2316",  # Norwood
-        # LK31
-        # "LK3136": "LK3136",  # Hikkaduwa - UNCHANGED
-        "LK3163": "LK3137",  # Rathgama
-        "LK3160": "LK3138",  # Madampagama
-        # "LK3127": "LK3127",  # Baddegama - UNCHANGED
-        "LK3157": "LK3128",  # Wanduramba
-        # LK91
-        # "LK9118": "LK9118",  # Balangoda - UNCHANGED
-        "LK9154": "LK9119",  # Kaltota
-    }
+    ID_CORRECTION_MAP = {}
 
     @classmethod
     def correct_v(cls, v):
