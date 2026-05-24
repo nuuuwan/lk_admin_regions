0a76da7b2d11add71e61606c6cd8c9e5f0c41741
nuuuwan <nuuuwan@gmail.com>
2026-05-24 16:03:22 +0530

    Refactor import statements and restore main function execution

--- workflows/pipeline.py
+++ workflows/pipeline.py
@@ -1,8 +1,13 @@
 import os
 
-from lk_admin_regions import (BuildEnts, BuildGeo, BuildNonAdminEnts,
-                              BuildNonAdminGeo, CombineDCSAndHumData,
-                              GNDListFinalXLSX)
+from lk_admin_regions import (
+    BuildEnts,
+    BuildGeo,
+    BuildNonAdminEnts,
+    BuildNonAdminGeo,
+    CombineDCSAndHumData,
+    GNDListFinalXLSX,
+)
 
 
 def main():
@@ -18,19 +23,5 @@ def main():
     os.system("find data -type f -size +25M -delete")
 
 
-def main_test():
-    # GNDListFinalXLSX.build_gnd_table()
-    # CombineDCSAndHumData.combine(dcs_region_label="gnd", hum_admin_level=4)
-
-    # BuildEnts.build_all()
-    # BuildGeo.build_all()
-
-    # BuildNonAdminEnts.build_all()
-    BuildNonAdminGeo.build_all()
-
-    os.system("find data -type f -size +25M -delete")
-
-
 if __name__ == "__main__":
-    # main()
-    main_test()
+    main()
