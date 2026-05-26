da7c808d60da0dcb1a7b622ced1babcdf74157b5
nuuuwan <nuuuwan@gmail.com>
2026-05-26 13:38:18 +0530

    Refactor pipeline: remove BuildNonAdminEnts import and call, add BuildGNDEnt build step

--- workflows/pipeline.py
+++ workflows/pipeline.py
@@ -1,18 +1,23 @@
 import os
 
-from lk_admin_regions import (BuildEnts, BuildGeo, BuildNonAdminEnts,
-                              BuildNonAdminGeo, CombineDCSAndHumData,
-                              GNDListFinalXLSX)
+from lk_admin_regions import (
+    BuildEnts,
+    BuildGeo,
+    BuildGNDEnt,
+    BuildNonAdminGeo,
+    CombineDCSAndHumData,
+    GNDListFinalXLSX,
+)
 
 
 def main():
     GNDListFinalXLSX.build_gnd_table()
     CombineDCSAndHumData.combine(dcs_region_label="gnd", hum_admin_level=4)
 
+    BuildGNDEnt.build()
     BuildEnts.build_all()
     BuildGeo.build_all()
 
-    BuildNonAdminEnts.build_all()
     BuildNonAdminGeo.build_all()
 
     os.system("find data -type f -size +25M -delete")
