a2990babc7ad5f0cc59e26569e33a5a3c0696c00
nuuuwan <nuuuwan@gmail.com>
2026-06-06 17:36:56 +0530

    Refactor build_region and build_all methods to handle splits as nested structures

--- src/lk_admin_regions/history/RegionsHistory.py
+++ src/lk_admin_regions/history/RegionsHistory.py
@@ -58,16 +58,15 @@ class RegionsHistory:
         os.makedirs(cls.DIR_DATA_ENTS_HISTORY, exist_ok=True)
         regions = BuildEnts.read(region_type_name)
 
-        for split_info in split_info_list:
-            regions = cls.split(
-                regions,
-                split_info["year"],
-                split_info["old_id"],
-                split_info["new_id"],
-            )
+        for year_info in split_info_list:
+            year = year_info["year"]
+            for split in year_info["splits"]:
+                regions = cls.split(
+                    regions, year, split["old_id"], split["new_id"]
+                )
             regions_path_base = os.path.join(
                 cls.DIR_DATA_ENTS_HISTORY,
-                f"{region_type_name}s-pre{split_info['year']}",
+                f"{region_type_name}s-pre{year}",
             )
             log.info(
                 f"Writing {len(regions)} {region_type_name}s"
@@ -79,10 +78,18 @@ class RegionsHistory:
     def build_all(cls):
         for region_type_name, split_info_list in dict(
             district=[
-                dict(year="1984", old_id="LK-41", new_id="LK-45"),
-                dict(year="1978", old_id="LK-11", new_id="LK-12"),
-                dict(year="1961", old_id="LK-51", new_id="LK-52"),
-                dict(year="1959", old_id="LK-81", new_id="LK-82"),
+                dict(
+                    year="1984", splits=[dict(old_id="LK-41", new_id="LK-45")]
+                ),
+                dict(
+                    year="1978", splits=[dict(old_id="LK-11", new_id="LK-12")]
+                ),
+                dict(
+                    year="1961", splits=[dict(old_id="LK-51", new_id="LK-52")]
+                ),
+                dict(
+                    year="1959", splits=[dict(old_id="LK-81", new_id="LK-82")]
+                ),
             ]
         ).items():
             cls.build_region(region_type_name, split_info_list)
