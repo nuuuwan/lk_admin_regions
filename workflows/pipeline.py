8292a087f51b1ab728575aafe456a31995e588be
nuuuwan <nuuuwan@gmail.com>
2026-06-08 19:00:36 +0530

    refactor: clean up import formatting and replace os.system call with BuildGeo.cleanup_big_files

--- workflows/pipeline.py
+++ workflows/pipeline.py
@@ -1,8 +1,12 @@
-import os
-
-from lk_admin_regions import (BuildEnts, BuildGeo, BuildGNDEnt,
-                              BuildNonAdminGeo, CombineDCSAndHumData,
-                              GNDListFinalXLSX, RegionsHistory)
+from lk_admin_regions import (
+    BuildEnts,
+    BuildGeo,
+    BuildGNDEnt,
+    BuildNonAdminGeo,
+    CombineDCSAndHumData,
+    GNDListFinalXLSX,
+    RegionsHistory,
+)
 
 
 def main():
@@ -16,7 +20,7 @@ def main():
 
     RegionsHistory.build_all()
 
-    os.system("find data -type f -size +25M -delete")
+    BuildGeo.cleanup_big_files()
 
 
 if __name__ == "__main__":
